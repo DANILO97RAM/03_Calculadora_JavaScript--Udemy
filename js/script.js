29b30551010a698a7ca33ddd855b466b074c07ea
DANILO97RAM <danilo.ramirez.gomez@outlook.es>
2022-11-14 12:08:07 -0500

    Digitos en pantalla de la calculadora, escribe numeros y signos

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var p = {
   teclas: document.querySelectorAll('#calculadora ul li'),
   // acciones de los botones
   accion: null,
+  digito: null,
+  // contador signos operaciones para no mostrar repetidos
+  cantidadSignos: 0,
+  operaciones: document.querySelector('#operaciones'),
 }
 
 //  Methods Object Calculator
@@ -13,34 +17,57 @@ var m = {
     for (var i=0; i< p.teclas.length; i++){
       // cuando se haga click, entonces:
       p.teclas[i].addEventListener('click',m.oprimirTecla)
-
     }
   },
-  // event captura todo las accion del click. el metodo target selecciona
+  // event captura todo las accion del click. El metodo target selecciona
   // el elemento, y desde target, llamamos la clase.
   oprimirTecla: function(event){
     p.accion = (event.target.getAttribute('class'));
+    p.digito = event.target.innerHTML; //capturar el contenido html   
+    
     // resolver todos los problemas con teclado y mouse
-    m.calculadora(p.accion)
+    m.calculadora(p.accion, p.digito)
   },
 
-  calculadora: function(accion){
+  calculadora: function(accion, digito){
     switch(accion){
       case 'numero':
-        console.log('numero');
+        p.cantidadSignos = 0;
+        if (p.operaciones.innerHTML == 0){
+          p.operaciones.innerHTML = digito;  
+        } else{
+          p.operaciones.innerHTML += digito;
+        }
         break
       case 'oper':
-        console.log('operacion');
+        //  cartucho de signos, cuantificar canridad de signos
+        p.cantidadSignos++
+        if (p.cantidadSignos == 1){
+          // pregunta para no poner signo de operacion cuando haya un cero
+          if (p.operaciones.innerHTML == 0){
+            p.operaciones = 0;
+          } else { //si es diferente de cero, agrega el signo de operacion
+            p.operaciones.innerHTML += digito;
+          }
+
+         
+        }
         break
       case 'decimal':
         console.log('decimal');
         break
       case 'resultado':
         console.log('resultado');
+        break
     }
+  },
+
+  borrarCalculadora: function(){
+    p.operaciones.innerHTML = 0;
   }
 
 }
 
 // inicializador de la calculadora
 m.inicio();
+
